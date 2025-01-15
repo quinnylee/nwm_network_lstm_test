a6ca787507d90d2748efd208f506d22c0e2c0646
slama0077 <slama6305@gmail.com>
2025-01-15 19:08:54 +0000

    slight improvement

--- data_collection_preprocess/generate_times.py
+++ data_collection_preprocess/generate_times.py
@@ -3,9 +3,9 @@ import glob
 year = input("Give me the year: \n")
 month = input('Give me the month:\n')
 
-print(month)
+if int(month) < 10:
+    month = str(0) + month
 number_of_days = int(len(glob.glob('/media/volume/Imp_Data/FORCING/' + year + '/' + year + month + '*'))/24)
-print(number_of_days)
 
 with open('times.txt', 'w') as file:
     for i in range(1, number_of_days + 1):
