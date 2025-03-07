35f6c9029e45ddcecbf3207c76c716c48559e005
slama0077 <slama6305@gmail.com>
2025-03-07 15:48:57 +0000

    added progress bar to get_q

--- data_collection_preprocess/Generate_NetCDF.py
+++ data_collection_preprocess/Generate_NetCDF.py
@@ -17,6 +17,7 @@ from exactextract import exact_extract
 import warnings
 import json
 from tqdm import tqdm  # Add tqdm
+import logging
 
 
 def read_key_value_file(filepath):
@@ -35,32 +36,44 @@ def get_q(qlat_files, id_list, value_col="streamflow"):
     with mp.Pool(processes=mp.cpu_count()) as pool:
         results = pool.starmap(
             process_chrtout_file2,
-            [(filename, id_list, value_col) for filename in qlat_files]
+            tqdm(
+                [(filename, id_list, value_col) for filename in qlat_files], 
+                total=len(qlat_files),
+                desc="Opening and subsetting CHRTOUT files")
         )
     combined = xr.concat(results, dim="time")
     return combined
 
-start = time.perf_counter()
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    start = time.perf_counter()
 
-file_path = "/media/volume/NeuralHydrology/Test_Quinn_Data/all_pairs.txt"
-key_value_pairs = read_key_value_file(file_path)
+    file_path = "/media/volume/NeuralHydrology/Test_Quinn_Data/all_pairs.txt"
+    logging.debug(f"file path: {file_path}")
+    key_value_pairs = read_key_value_file(file_path)
 
-comids = []
+    comids = []
 
-for i, (key, value) in enumerate(key_value_pairs):
-    comids.append(key)
-    comids.append(value)
+    for i, (key, value) in enumerate(key_value_pairs):
+        comids.append(key)
+        comids.append(value)
 
-comids = [3441628, 3438398]
-q_dataset = get_q(glob.glob("/mnt/IMP_DATA/CHRTOUT/2008/*.CHRTOUT_DOMAIN1"), comids)
-target_directory = '/media/volume/NeuralHydrology/Test_Quinn_Data/Channel_Routing/2008/'
-feature_id_list = q_dataset['feature_id'].values
+    comids = [3441628, 3438398]
+    logging.debug(f'IDs: {comids}')
+    q_dataset = get_q(glob.glob("/mnt/IMP_DATA/CHRTOUT/2008/*.CHRTOUT_DOMAIN1"), comids)
+    logging.info("finished getting one year of streamflow")
+    target_directory = '/media/volume/NeuralHydrology/Test_Quinn_Data/Channel_Routing/2008/'
+    feature_id_list = q_dataset['feature_id'].values
 
-# Add tqdm progress bar here
-for i in tqdm(range(len(feature_id_list)), desc="Saving NetCDF files"):
-    file_path = target_directory + str(feature_id_list[i]) + '.nc'
-    q_dataset.isel(feature_id=i).to_netcdf(file_path)
+    logging.info("starting process")
+    # Add tqdm progress bar here
+    for i in tqdm(range(len(feature_id_list)), desc="Saving NetCDF files"):
+        file_path = target_directory + str(feature_id_list[i]) + '.nc'
+        q_dataset.isel(feature_id=i).to_netcdf(file_path)
 
-end = time.perf_counter()
+    end = time.perf_counter()
 
-print(f"Operation took {end - start:.2f} seconds")
+    print(f"Operation took {end - start:.2f} seconds")
+
+if __name__ == "__main__":
+    main()
